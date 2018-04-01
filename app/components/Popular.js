41442b08bcd328f1d5952227540fd7680bf06321
Luis Pineda JR <lspnd00@gmail.com>
2018-04-01 17:06:09 -0400

    added state fn component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,4 +1,29 @@
 var React = require('react');
+var PropTypes = require('prop-types');
+
+function  SelectedLanguage (props){
+  var languages = [ 'All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python' ];
+  return (
+    <ul className ='languages'>
+
+        {languages.map(function (lang){
+          return(
+              <li
+                style = { lang === props.selectedLanguage ? { color: '#d0021b'}: null }
+                onClick = {props.onSelect.bind(null, lang)}
+                key = {lang}>
+                    {lang}
+              </li>
+      )
+    })}
+    </ul>
+  )
+}
+
+  SelectedLanguage.propTypes = {
+    selectedLanguage: PropTypes.string.isRequired,
+    onSelect: PropTypes.func.isRequired,
+  }
 
 class Popular extends React.Component{
   constructor (props){
@@ -18,22 +43,12 @@ updateLanguage(lang){
 }
 
   render(){
-    var languages = [ 'All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python' ];
-
     return (
-      <ul className ='languages'>
-
-          {languages.map(function (lang){
-            return(
-              <li
-              style = { lang === this.state.selectedLanguage ? { color: '#d0021b'}: null }
-              onClick = {this.updateLanguage.bind(null, lang)}
-              key = {lang}>
-                  {lang}
-              </li>
-            )
-          }, this )}
-      </ul>
+        <div>
+          <SelectedLanguage
+          selectedLanguage = {this.state.selectedLanguage}
+          onSelect = { this.updateLanguage}/>
+        </div>
     )
   }
 }
